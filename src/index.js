ef94975c8e3fdd56c24f3768105150185fe4b074
amanda-gomes <amacrisgomes@hotmail.com>
2019-05-04 16:50:23 -0300

    função cpfValidator funcionado para strigs com pontos e traços e numbers

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-function validCPF(getNumber) {
-  const cpfWithoutCharacter = getNumber.replace(/[^\d]+/g, '');
-  return numberofDigits(cpfWithoutCharacter);
-}
-
-function numberofDigits(number) {
-  const digits = number.length;
-
-  if (digits < 11 || digits > 11) {
-    return false;
+function cpfValidator(CPF) {
+  if (typeof (CPF) === 'number') {
+    let numberToString = CPF.toString();
+    let valueWithoutCharacteres = numberToString.replace(/[^\d]+/g, '');
+    return numberofDigits(valueWithoutCharacteres);
   } else {
-    return equalDigits(number);
+    let valueWithoutCharacteres = CPF.replace(/[^\d]+/g, '');
+    return numberofDigits(valueWithoutCharacteres);
   }
 }
 
-function equalDigits(elevenDigits) {
+function numberofDigits(CPF) {
+  const digits = CPF.length;
+  if (digits < 11 || digits > 11) {
+    return false;
+  } else {
+    return equalDigits(CPF);
+  }
+}
+
+function equalDigits(CPF) {
   let numbersEquals = 0;
-  for (let digit of elevenDigits) {
-    if (elevenDigits[0] === digit) {
+  for (let digit of CPF) {
+    if (CPF[0] === digit) {
       numbersEquals++;
     }
   }
@@ -24,17 +29,17 @@ function equalDigits(elevenDigits) {
   if (numbersEquals === 11) {
     return false;
   } else {
-    return firsDigit(elevenDigits);
+    return firsDigit(CPF);
   }
 }
 
 function firsDigit(CPF) {
   let sum = 0;
   let multiplier = 10;
-  let nineDigits = CPF.substring(0, 9);
+  let nineFirstNumbers = CPF.substring(0, 9);
   let firstDigitChecker = parseInt(CPF.substring(9, 10));
-
-  for (let digit of nineDigits) {
+  // substituir por reduce
+  for (let digit of nineFirstNumbers) {
     sum = sum + (digit * multiplier);
     multiplier--;
   }
@@ -58,10 +63,10 @@ function firsDigit(CPF) {
 function seconDigit(CPF) {
   let sum = 0;
   let multiplier = 11;
-  let teenDigits = CPF.substring(0, 10);
+  let teenFirstNumbers = CPF.substring(0, 10);
   let seconDigitChecker = parseInt(CPF.substring(10, 11));
-
-  for (let digit of teenDigits) {
+  // substituir por reduce
+  for (let digit of teenFirstNumbers) {
     sum = sum + (digit * multiplier);
     multiplier--;
   }
@@ -82,4 +87,4 @@ function seconDigit(CPF) {
   }
 }
 
-module.exports.validCPF = validCPF;
+module.exports.cpfValidator = cpfValidator;
--- test/index.spec.js
+++ test/index.spec.js
@@ -4,37 +4,42 @@ const expect = chai.expect;
 const valid = require('../src/index');
 
 describe('valid cpf lib', () => {
-  describe('validCPF', () => {
+  describe('cpfValidator', () => {
+    it('contém letras', () => {
+      expect(valid.cpfValidator('AAA156151H6')).to.equal(false);
+    });
     it('menos que 11 dígitos', () => {
-      expect(valid.validCPF('0101927678')).to.equal(false);
+      expect(valid.cpfValidator('0101927678')).to.equal(false);
     });
     it('mais que 11 dígitos', () => {
-      expect(valid.validCPF('7829279262543')).to.equal(false);
-    });
-    it('contém letras', () => {
-      expect(valid.validCPF('AAA156151H6')).to.equal(false);
-    });
+      expect(valid.cpfValidator('7829279262543')).to.equal(false);
+    });    
     it('dígitos iguais', () => {
-      expect(valid.validCPF('11111111111')).to.equal(false);
+      expect(valid.cpfValidator('11111111111')).to.equal(false);
     });
     it('primeiro dígito verificador incorreto', () => {
-      expect(valid.validCPF('12345678910')).to.equal(false);
+      expect(valid.cpfValidator('12345678910')).to.equal(false);
     });
     it('segundo dígito verificador incorreto', () => {
-      expect(valid.validCPF('12345678908')).to.equal(false);
+      expect(valid.cpfValidator('12345678908')).to.equal(false);
     });
     it('cpf válido', () => {
-      expect(valid.validCPF('12345678909')).to.equal(true);
+      expect(valid.cpfValidator('12345678909')).to.equal(true);
     });
     it('cpf válido', () => {
-      expect(valid.validCPF('66225711040')).to.equal(true);
+      expect(valid.cpfValidator('66225711040')).to.equal(true);
     });
     it('cpf inválido com pontos e traço', () => {
-      expect(valid.validCPF('190.678.567-00')).to.equal(false);
+      expect(valid.cpfValidator('190.678.567-00')).to.equal(false);
     });
     it('cpf válido com pontos e traço', () => {
-      expect(valid.validCPF('461.469.950-29')).to.equal(true);
+      expect(valid.cpfValidator('461.469.950-29')).to.equal(true);
     });
-
+    it('cpf válido em formato de número', () => {
+      expect(valid.cpfValidator(18881841088)).to.equal(true);
+    });
+    it('cpf inválido em formato de número', () => {
+      expect(valid.cpfValidator(30823229000)).to.equal(false);
+    });
   });
 });
